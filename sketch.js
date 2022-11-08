35df03c2f6f8c9af23f6d82fc18e5b52098c040c
tbdlarsen <105425030+tbdlarsen@users.noreply.github.com>
2022-11-08 13:48:31 +0100

    lavet grid funktion

--- sketch.js
+++ sketch.js
@@ -3,11 +3,19 @@ function setup() {
 }
 
 function draw() {
-  background(120);
+  background(220);
+  grid();
 }
 
 function grid(){
-  
+
+  for (var x =0; x < width * (4/5); x += width/15) {
+    stroke(0);
+		strokeWeight(1);
+		line(x, 0, x, height);
+
+  }
+
 
 
 
