d60cc4ddba85268100b4af080656de2c94518bb3
tbdlarsen <105425030+tbdlarsen@users.noreply.github.com>
2022-11-08 14:22:24 +0100

    rækker og kolonner

--- sketch.js
+++ sketch.js
@@ -9,10 +9,15 @@ function draw() {
 
 function grid(){
 
-  for (var x = 0; x <= width; x += width/10) {
-    stroke(0);
-		strokeWeight(1);
-		line(x, 0, x, height);
+  for (var x = 0; x < width; x += width/10) {
+    for(var y = 0; y < height; y += height/20){
+      
+      stroke(0);
+		  strokeWeight(1);
+		  line(x, 0, x, height);
+      line(0, y, width, y);
+
+    }
 
   }
 
